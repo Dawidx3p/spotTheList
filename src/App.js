e5b9cb9ab49bb80cba237f7c135c3773af91dd70
Dawidx3p <dawiddebiecx@gmail.com>
2020-11-18 22:16:06 +0100

    add react bootstrap and bootstrap files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-
+import {Container, Row, Col} from 'react-bootstrap/'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <img src={logo} alt="logo" id="logo"/>
+      <Container>
+        <Row>
+          <Col xs={12} md={4}>
+          poziomka1
+          </Col>
+          <Col xs={12} md={4}>
+          poziomka1
+          </Col>
+          <Col xs={12} md={4}>
+          poziomka1
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
